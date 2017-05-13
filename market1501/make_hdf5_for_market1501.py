d6154a311ce1aa4e86cc10a1182022805e9e1fad
yumkong <yumkongleo@gmail.com>
2017-05-13 15:24:40 -0400

    fix test bug

--- market1501/make_hdf5_for_market1501.py
+++ market1501/make_hdf5_for_market1501.py
@@ -13,18 +13,24 @@ def make_positive_index_market1501(train_or_test = 'train',user_name = 'ubuntu')
     i = 0
     while i < len(path_list):
         j = i + 1
+        # e.g.: 0002_c3s1_....
+        # 0513: the first 4digit is the identity number
         while j < len(path_list) and path_list[j][0:4] == path_list[i][0:4]:
+            # the 7th digit is the camera number, images should be taken from different cameras
             if path_list[j][6] != path_list[i][6]:
+                #since using assymmetry comparison, use both (a,b) and (b,a)
                 index.append([path_list[i],path_list[j]])
                 index.append([path_list[j],path_list[i]])
                 print len(index)
             j += 1
         i += 1
     print 'transforming the list to the numpy array......'
+    # convert from list to a 1xN matrix
     index = np.array(index)
     print 'shuffling the numpy array......'
     np.random.shuffle(index)
     print 'storing the array to HDF5 file......'
+    # [1] dataset name, [2] dataset content
     f.create_dataset(train_or_test,data = index)
 
 
@@ -33,10 +39,13 @@ def make_test_hdf5(user_name='ubuntu'):
         path_list = get_image_path_list(train_or_test='test')
         #0512: delete the last thumbs.db
         #path_list.pop(-1)
-        i = path_list[0][0:4]
-        c = path_list[0][6]
+        i = path_list[0][0:4]  #identity
+        c = path_list[0][6]    #camera
         temp = []
+        # 0512: using identity id as group name e.g.: "0001"
         fi = f.create_group(i)
+        
+        #use the following for loop to create a group for each of the 1501 identity
         for path in path_list:
             print path
             if path[0:4] == i:
@@ -44,6 +53,7 @@ def make_test_hdf5(user_name='ubuntu'):
                     temp.append(np.array(Image.open('./dataset/market1501/bounding_box_test/' + path)))
                 else:
                     print len(temp)
+                    # 0512: use camera id as the dataset name of this group
                     fi.create_dataset(c,data = np.array(temp))
                     temp = []
                     c = path[6]
@@ -52,6 +62,7 @@ def make_test_hdf5(user_name='ubuntu'):
             else:
                 fi.create_dataset(c,data = np.array(temp))
                 i = path[0:4]
+                # 0512: create a new group according to the next identity
                 fi = f.create_group(i)                
                 print i
                 c = path[6]
@@ -71,9 +82,11 @@ def get_image_path_list(train_or_test = 'train',system_user_name = 'ubuntu'):
     assert os.path.isdir(folder_path)
     if train_or_test == 'train' or train_or_test == 'query':
         #return sorted(os.listdir(folder_path))
+        #sort the list in ascending order in ascII names
         path_list = sorted(os.listdir(folder_path))
     elif train_or_test == 'test':
         #return sorted(os.listdir(folder_path))[6617:]
+        # 0513: start from 6618th image, the first 6617 imgs are distractors or junks of DPM detector
         path_list = sorted(os.listdir(folder_path))[6617:]
     #0512: delete the last thumbs.db
     path_list.pop(-1)
